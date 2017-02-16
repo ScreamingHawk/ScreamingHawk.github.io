f4f566e7e9742d9a41b0d8275d7b0e01ab9eb052
Michael Standen <screaminghawk@gmail.com>
2017-02-17 12:32:57 +1300

    Automatically fill url for testing and building

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+sed -i.bak 's/url:.*/url: https:\/\/michael.standen.link/' _config.yml
+rm _config.yml.bak
 bundle exec jekyll build
-cd _sites
+cd _site
 rm *.sh
--- serve.sh
+++ serve.sh
@@ -1,1 +1,3 @@
+sed -i.bak 's/url:.*/url: http:\/\/localhost:4000/' _config.yml
+rm _config.yml.bak
 bundle exec jekyll serve
