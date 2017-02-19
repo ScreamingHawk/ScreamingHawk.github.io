d37f878969c0a92ca017a3e168a53f2802727192
Michael Standen <screaminghawk@gmail.com>
2017-02-20 12:15:14 +1300

    Correct sed use

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+sed -i.bak 's/url:.*/url: https:\/\/michael.standen.link/' _config.yml
+rm _config.yml.bak
 #npm install -g uglify-js
 uglifyjs assets/js/src/* -o assets/js/main.min.js # Minify javascript
 rm -r _site # Force complete build
