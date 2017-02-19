ab37acd745da012eb5a459faf9c51b94188b6a25
Michael Standen <screaminghawk@gmail.com>
2017-02-19 20:37:50 +1300

    minify js on build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#sed -i.bak 's/url:.*/urls: https:\/\/michael.standen.link/' _config.yml
-#rm _config.yml.bak
+#npm install -g uglify-js
+uglifyjs assets/js/src/* -o assets/js/main.min.js # Minify javascript
 rm -r _site # Force complete build
 bundle exec jekyll build
